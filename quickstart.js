7a4be3c32b26975f04b260811e14c158dd2a9cb2
haint <haithanh0809@gmail.com>
2017-08-02 23:21:32 +0700

    test send gmail

--- quickstart.js
+++ quickstart.js
@@ -1,8 +1,14 @@
 var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
+var OAuth2Client = google.auth.OAuth2;
 
-var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var SCOPES = [
+  'https://mail.google.com/',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/gmail.send'
+];
 var TOKEN_DIR = '.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
 
@@ -12,64 +18,72 @@ fs.readFile('client_secret.json', (err, content) => {
     return;
   }
 
-  authorize(JSON.parse(content), listLabels);
+  authorize(JSON.parse(content), sendMessage);
 });
+
+var getAccessToken = (oauth2Client, callback) => {
+  var url = oauth2Client.generateAuthUrl({
+    access_type: 'online',
+    scope: SCOPES
+  });
+
+  console.log('Visit the url: ', url);
+  
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question('Enter the code here:', function(code) {
+    oauth2Client.getToken(code, (err, tokens) => {
+      if (err) {
+        return callback(err);
+      }
+      oauth2Client.setCredentials(tokens);
+
+      console.log('Oauth2 Token: ', tokens);
+
+      callback(oauth2Client);
+    });
+  })
+}
 
 var authorize = (credentials, callback) => {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
   var redirectUrl = credentials.installed.redirect_uris[0];
 
-  // console.log(credentials);
-  // console.log(google.auth.OAuth2);
-
-  // var auth = googleAuth();
-  var oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
+  var oauth2Client = new OAuth2Client(clientId, clientSecret, redirectUrl);
+  getAccessToken(oauth2Client, callback);
 }
 
-var getNewToken = (oauth2Client, callback) => {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oauth2Client.getToken(code, (err, token) => {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
+var makeBody = (to, from, subject, message) => {
+  var str = [
+    "Content-Type: text/plain; charset=\"UTF-8\"\n",
+    "MIME-Version: 1.0\n",
+    "Content-Transfer-Encoding: 7bit\n",
+    "to: ", to, "\n",
+    "from: ", from, "\n",
+    "subject: ", subject, "\n\n",
+    message
+  ].join('');
+
+  var encodedMail = new Buffer(str).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+  return encodedMail;
+}
+
+var sendMessage = (auth) => {
+  var raw = makeBody("haithanh0809@gmail.com", "haithanh0809@gmail.com", "test subject", "test message");
+  var gmail = google.gmail('v1');
+  gmail.users.messages.send({
+    auth:auth,
+    userId: 'me',
+    resource: {
+      raw: raw
+    }
+  }, (err, resp) => {
+    console.log(err || resp);
   })
-}
-
-var storeToken = (token) => {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
 }
 
 var listLabels = (auth) => {
@@ -92,6 +106,5 @@ var listLabels = (auth) => {
         console.log('- %s', label.name);
       }
     }
-    
   })
 }
